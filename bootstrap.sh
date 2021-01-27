649806a69f5ebc64430d462c03a912702b2dda8c
Trevor Royer <troyer@redhat.com>
2021-01-27 13:16:16 -0600

    basic bootstrap of argo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
-argo_folder="./clusters/base/operators/argo"
+argo_folder="./clusters/base/operators/argocd"
 app_folder="./clusters/overlays/4.5/applications"
 
+argo_address="argocd-server-argocd.apps.cluster-0183.0183.example.opentlc.com"
+cluster_address="default/api-cluster-0183-0183-example-opentlc-com:6443/opentlc-mgr"
+
+# install the argo operator
 oc apply -k "${argo_folder}"
 
+# configure the cluster with argo
+argocd login --sso "${argo_address}"
+argocd cluster add "${cluster_address}"
+
+# setup the applications folder to bootstrap everything
 oc apply -k "${app_folder}"
