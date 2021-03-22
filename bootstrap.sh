1cdea1c4f5c9e883ad079b013714e19354e0bbdb
Trevor Royer <troyer@redhat.com>
2021-03-22 16:56:27 -0500

    fix ref

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,3 +15,41 @@ argocd cluster add "${cluster_address}"
 
 # setup the applications folder to bootstrap everything
 oc apply -f "${app_of_apps}"
+
+
+#!/bin/bash
+
+LANG=C
+SLEEP_SECONDS=45
+
+echo ""
+echo "Creating ArgoCD Project"
+# Avoids weird race condition where sometimes two installplans get created
+oc new-project argocd
+sleep 2
+
+echo ""
+echo "Installing Argo CD Operator."
+
+oc apply -k clusters/base/operators/argocd-operator/
+
+echo "Pause $SLEEP_SECONDS seconds for the creation and approval of the InstallPlan."
+sleep $SLEEP_SECONDS
+
+oc rollout status deploy/argocd-operator -n argocd
+
+echo "Listing Argo CD CRDs."
+oc get crd | grep argo
+
+
+echo "Deploying Argo CD instance"
+
+oc apply -k argocd/overlays/default
+
+echo "Waiting for Argo CD server to start..."
+
+sleep $SLEEP_SECONDS
+
+oc rollout status deploy/argocd-server -n argocd
+
+echo "Argo CD ready!"
