f7290be18f77ec1fc3101496fa70b5b9e21139f2
Trevor Royer <troyer@redhat.com>
2021-03-23 09:30:44 -0500

    update bootstrap to use kustomize

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,3 @@
-argo_folder="./clusters/base/operators/argocd"
-app_of_apps="./clusters/base/applications/applications-app.yaml"
-
-argo_address="argocd-server-argocd.apps.cluster-0183.0183.example.opentlc.com"
-cluster_address="default/api-cluster-0183-0183-example-opentlc-com:6443/opentlc-mgr"
-
-# install the argo operator
-oc apply -k "${argo_folder}"
-
-# need to configure the git repo tokens
-
-# configure the cluster with argo
-argocd login --sso "${argo_address}"
-argocd cluster add "${cluster_address}"
-
-# setup the applications folder to bootstrap everything
-oc apply -f "${app_of_apps}"
-
-
 #!/bin/bash
 
 LANG=C
@@ -31,7 +12,8 @@ sleep 2
 echo ""
 echo "Installing Argo CD Operator."
 
-oc apply -k clusters/base/operators/argocd-operator/
+#oc apply -k clusters/base/operators/argocd/argocd-operator/
+kustomize build clusters/base/operators/argocd/argocd-operator/ | oc apply -f -
 
 echo "Pause $SLEEP_SECONDS seconds for the creation and approval of the InstallPlan."
 sleep $SLEEP_SECONDS
@@ -41,10 +23,10 @@ oc rollout status deploy/argocd-operator -n argocd
 echo "Listing Argo CD CRDs."
 oc get crd | grep argo
 
-
 echo "Deploying Argo CD instance"
 
-oc apply -k argocd/overlays/default
+# oc apply -k clusters/base/operators/argocd/argocd/
+kustomize build clusters/base/operators/argocd/argocd/ | oc apply -f -
 
 echo "Waiting for Argo CD server to start..."
 
@@ -53,3 +35,5 @@ sleep $SLEEP_SECONDS
 oc rollout status deploy/argocd-server -n argocd
 
 echo "Argo CD ready!"
+
+# oc apply -k clusters/overlays/
